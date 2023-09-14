5706c6d3202bddc27133f62b6f852aac2d8788e3
613668 <beres.613668@swl.k12.oh.us>
2023-09-14 09:34:05 -0400

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variables
+        int intOperandA = 7;
+        int intOperandB = 3;
+        int intSum;
+        int intDifference;
+        int intProduct;
+        int intQuotient;
+        int intModulo;
+        double doubleOperandA = 5.5;
+        double doubleOperandB = 4.5;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleModul;
+
+        //operations
+        System.out.println("intSum = " + (intOperandA + intOperandB));
+        System.out.println("intDifference = " + (intOperandA - intOperandB));
+        System.out.println("intProduct = " + (intOperandA * intOperandB));
+        System.out.println("intQuotient = " + (intOperandA / intOperandB));
+        System.out.println("intModulo = " + (intOperandA % intOperandB));
+
+        System.out.println("doubleSum = " + (doubleOperandA + doubleOperandB));
+        System.out.println("doubleDifference = " + (doubleOperandA - doubleOperandB));
+        System.out.println("doubleProduct = " + (doubleOperandA * doubleOperandB));
+        System.out.println("doubleQuotient = " + (doubleOperandA / doubleOperandB));
+        System.out.println("doubleModulo = " + (doubleOperandA % doubleOperandB));
     }
 }
